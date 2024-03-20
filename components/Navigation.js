c7f3abb586628cd5077705b6db39ec8b175cb064
Amza Omiteru <amza.web@gmail.com>
2024-03-20 16:45:01 +0000

    finished up

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,49 +1,75 @@
-'use client'
-import Link from 'next/link'
-import React from 'react'
-import Image from 'next/image'
-import { useState } from 'react'
+"use client"
+import Link from "next/link"
+import React from "react"
+import Image from "next/image"
+import { useState } from "react"
 
-import menuBtn from '@/app/assets/svg/menu.svg'
-import xMark from '@/app/assets/svg/xmark.svg'
+import menuBtn from "@/app/assets/svg/menu.svg"
+import xMark from "@/app/assets/svg/xmark.svg"
 
 const Navigation = () => {
-    const [navOpened, setnavOpened] = useState(false)
-    const openNav = (inp) => {
-        setnavOpened(true)
-        }
-    
-    const closeNav = (inp) => {
-        setnavOpened(false)
-    }
-    
-    return (
-        <header className='bg-white py-7 px-11 pl-12 flex justify-between shadow-pr fixed top-0 w-full z-50'>
-            <div>
-                <h3 className="logo">Amza.dev</h3>
-            </div>
-            <nav className={navOpened ? "nav left-0" : "nav -left-[100vw]"}>
-                <div className='relative'>
-                    <button className='absolute right-10 top-8' onClick={closeNav}>
-                        <Image src={xMark} alt='logo' height={33} />
-                    </button>
-                </div>
-                <div className='flex justify-center h-full items-center text-2xl'>
-                    <ul className='flex flex-col gap-10 text-center'>
-                        <li><Link href={'#'}>Home</Link></li>
-                        <li><Link href={'#'}>About</Link></li>
-                        <li><Link href={'#'}>Portfolio</Link></li>
-                        <li><Link href={'#'}>Contact</Link></li>
-                    </ul>
-                </div>
-            </nav>
-            <div>
-                <button onClick={openNav}>
-                    <Image src={menuBtn} alt='logo' width={23} height={23} />
-                </button>
-            </div>
-        </header>
-    )
+  const [navOpened, setnavOpened] = useState(false)
+  const openNav = (inp) => {
+    setnavOpened(true)
+  }
+
+  const closeNav = (inp) => {
+    setnavOpened(false)
+  }
+
+  return (
+    <header className="bg-white py-7 px-11 pl-12 flex justify-between shadow-pr fixed top-0 w-full z-50">
+      <div>
+        <h3 className="logo">Amza.dev</h3>
+      </div>
+      <nav className={navOpened ? "nav left-0" : "nav -left-[100vw]"}>
+        <div className="relative">
+          <button className="absolute right-10 top-8" onClick={closeNav}>
+            <Image src={xMark} alt="logo" height={33} />
+          </button>
+        </div>
+        <div className="flex justify-center h-full items-center text-2xl">
+          <ul className="flex flex-col gap-10 text-center">
+            <li>
+              <Link href={"#"}>Home</Link>
+            </li>
+            <li>
+              <Link href={"#"}>About</Link>
+            </li>
+            <li>
+              <Link href={"#"}>Portfolio</Link>
+            </li>
+            <li>
+              <Link href={"#"}>Contact</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <nav className="md:hidden sm:hidden lg:block">
+        <div className="flex justify-center h-full items-center">
+          <ul className="flex flex-row gap-10 text-center">
+            <li>
+              <Link href={"#"}>Home</Link>
+            </li>
+            <li>
+              <Link href={"#"}>About</Link>
+            </li>
+            <li>
+              <Link href={"#"}>Portfolio</Link>
+            </li>
+            <li>
+              <Link href={"#"}>Contact</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <div className="lg:hidden">
+        <button onClick={openNav}>
+          <Image src={menuBtn} alt="logo" width={23} height={23} />
+        </button>
+      </div>
+    </header>
+  )
 }
 
 export default Navigation
--- components/Project.js
+++ components/Project.js
@@ -1,32 +1,60 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import Image from "next/image"
+import Link from "next/link"
+import React from "react"
 
-import github from '@/app/assets/svg/github.svg'
-import arrow from '@/app/assets/svg/arrow.svg'
+import github from "@/app/assets/svg/github.svg"
+import arrow from "@/app/assets/svg/arrow.svg"
 
-const Project = ({project}) => {
+const Project = ({ project }) => {
   return (
-    <div className='project md:mx-auto md:max-w-xl lg:max-w-none lg:flex lg:flex-row lg:justify-center lg:gap-16'>
-        <div className="img-cont shadow-pr lg:basis-3/5">
-            <Image src={project.image} alt='proj1' />
+    <div className="project md:mx-auto md:max-w-xl lg:max-w-none lg:flex lg:flex-row lg:justify-center lg:gap-16">
+      <div className="img-cont shadow-pr lg:basis-3/5">
+        <Image src={project.image} alt="proj1" />
+      </div>
+      <div className="px-5 md:max-w-xs md:mx-auto lg:basis-2/5 lg:max-w-none lg:py-7">
+        <h4 className="mb-5">{project.name}</h4>
+        <p className="lg:text-base">{project.desc}</p>
+        <ul className="mt-5 text-base font-semibold">
+          {project.involvedTech.map((skill, index) => {
+            return <li key={index}>{skill}</li>
+          })}
+        </ul>
+        <div
+          className={
+            project.nocodeLink
+              ? "justify-center mt-10"
+              : "flex gap-2.5 justify-evenly mt-10"
+          }
+        >
+          {project.nocodeLink ? (
+            <span></span>
+          ) : (
+            <Link
+              className="flex gap-1.5 items-center justify-center "
+              href={project.codeLink}
+            >
+              Code{" "}
+              <Image
+                className="h-[1.5em] w-min"
+                src={github}
+                alt="github icon"
+              />{" "}
+            </Link>
+          )}
+
+          <Link
+            className="flex gap-1.5 items-center justify-center "
+            href={project.liveLink}
+          >
+            Live Demo
+            <Image
+              className="h-[1.5em] w-min"
+              src={arrow}
+              alt="foward icon"
+            />{" "}
+          </Link>
         </div>
-        <div className='px-5 md:max-w-xs md:mx-auto lg:basis-2/5 lg:max-w-none lg:py-7'>
-            <h4 className='mb-5'>{project.name}</h4>
-            <p className='lg:text-base'>
-                {project.desc}
-            </p>
-            <ul className='mt-5 text-base font-semibold'>
-                {project.involvedTech.map((skill, index)=>{
-                    
-                    return <li key={index}>{skill}</li>
-                })}
-            </ul>
-            <div className='flex gap-2.5 justify-evenly mt-10'>
-                <Link className='flex gap-1.5 items-center justify-center ' href={'#'}>Code <Image className='h-[1.5em] w-min' src={github} alt='github icon' />  </Link>
-                <Link className='flex gap-1.5 items-center justify-center ' href={'#'}>Live Demo<Image className='h-[1.5em] w-min' src={arrow} alt='foward icon' />  </Link>
-            </div>
-        </div>
+      </div>
     </div>
   )
 }
